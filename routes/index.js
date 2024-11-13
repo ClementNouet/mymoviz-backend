b61533b888b786946950240705a4ab0f2feba432
Clément N <nouet.clem@gmail.com>
2024-11-13 12:51:27 +0100

    deploiement vercel

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 var express = require('express');
 var router = express.Router();
 
+const apiKey = process.env.REACT_APP_API_KEY;
+
 router.get('/movies', (req, res) => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?include_video=false&language=fr-FR&page=1&sort_by=popularity.desc&api_key=${process.env.apiKey}`)
+    fetch(`https://api.themoviedb.org/3/discover/movie?include_video=false&language=fr-FR&page=1&sort_by=popularity.desc&api_key=${apiKey}`)
         .then(response => response.json())
         .then(data => {
             res.json({result: true, movies: data.results});
